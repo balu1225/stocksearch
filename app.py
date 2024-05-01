2ab75b799fe1644127e90fe72d5c16796b380114
balu1225 <60847350+balu1225@users.noreply.github.com>
2024-05-01 16:45:18 -0700

    updating_apis

--- app.py
+++ app.py
@@ -1,24 +1,42 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request ,render_template
 import requests
 
 app = Flask(__name__)
 
-@app.route('/tiingo/aapl', methods=['GET'])
-def get_data():
-    headers = {'Content-Type': 'application/json'}
-      # Replace with your Tiingo token
-    url = f"https://api.tiingo.com/tiingo/daily/aapl?token=e02ffcf8c4bb38207f3acd04d9574bcf5522f91e"
-    
-    # Make the request to Tiingo API
-    response = requests.get(url, headers=headers)
-    
-    # Check if the request was successful
+API_KEY = 'e02ffcf8c4bb38207f3acd04d9574bcf5522f91e'
+TIINGO_API_URL = 'https://api.tiingo.com/tiingo/daily/'
+IEX_API_URL = 'https://api.tiingo.com/iex/'
+
+@app.route('/')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/tiingo')
+def get_tiingo_data():
+    ticker = 'aapl'
+    url = f'{TIINGO_API_URL}{ticker}'
+    params = {'token': API_KEY}
+    response = requests.get(url, params=params)
+
     if response.status_code == 200:
-        # Return the JSON response from Tiingo
         return jsonify(response.json())
     else:
-        # Return an error message if the request failed
-        return jsonify({'error': 'Failed to fetch data from Tiingo'}), response.status_code
+        return jsonify({'error': 'Failed to fetch data from the Tiingo API', 'status_code': response.status_code}), 500
+
+@app.route('/iex')
+def get_iex_data():
+    params = {
+        'tickers': 'aapl',
+        'token': API_KEY
+    }
+    response = requests.get(IEX_API_URL, params=params)
+
+    if response.status_code == 200:
+        return jsonify(response.json())
+    else:
+        return jsonify({'error': 'Failed to fetch data from the IEX API', 'status_code': response.status_code}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
